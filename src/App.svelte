d7719d381627869001be083122543edf9d20ac82
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2022-10-17 13:28:55 -0600

    Learned about dynamic variables

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,15 @@
 	let name = 'Jose';
 	let age = 24;
 
+    // let uppercaseName; is not required
+    $: uppercaseName = name.toUpperCase();
+
 	function incrementAge() {
 		age += 1;
 	}
+    function changeName() {
+        name = "Jose Ramiro";
+    }
 </script>
 
 <style>
@@ -13,5 +19,6 @@
 	}
 </style>
 
-<h1>Hello {name.toUpperCase()}, my age is {age + 1}!</h1>
+<h1>Hello {uppercaseName}, my age is {age + 1}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change Name</button>
