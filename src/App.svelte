4c1a905679b42639be320523f37ad1168613f892
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2022-10-17 14:01:11 -0600

    binding elements

--- src/App.svelte
+++ src/App.svelte
@@ -18,6 +18,10 @@
     function changeName() {
         name = 'Jose Ramiro';
     }
+    function nameInput(event) {
+        const enteredValue = event.target.value;
+        name = enteredValue
+    }
 </script>
 
 <style>
@@ -28,4 +32,5 @@
 
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
-<button on:click="{changeName}">Change Name</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<input type="text" value="{name}" on:input="{nameInput}">
