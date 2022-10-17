6fa7e0a2ac2d23276b134493fdb355dec9d92f32
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2022-10-17 13:13:27 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Jose';
+	let age = 24;
+
+	function incrementAge() {
+		age += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name.toUpperCase()}, my age is {age + 1}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
