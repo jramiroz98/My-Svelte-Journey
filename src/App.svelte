5368fb60e4266b19e8ddc1a361d3546b6021d01a
Jose Ramiro Zuñiga <jramiroz98@gmail.com>
2022-10-22 22:51:21 -0600

    Using multiple components

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,41 @@
 <script>
-	let name = 'Jose';
-	let age = 24;
+  import ContactCard from "./ContactCard.svelte"
 
-    // let uppercaseName; is not required
-    $: uppercaseName = name.toUpperCase();
+  let name = "Jose"
+  let age = 24
 
-    $: console.log(name);
+  // let uppercaseName; is not required
+  $: uppercaseName = name.toUpperCase()
 
-    $: if (name === 'Jose Ramiro') {
-        console.log('It runs!');
-        age = 31;
-    }
+  $: console.log(name)
 
-	function incrementAge() {
-		age += 1;
-	}
-    function changeName() {
-        name = 'Jose Ramiro';
-    }
-    function nameInput(event) {
-        const enteredValue = event.target.value;
-        name = enteredValue
-    }
+  $: if (name === "Jose Ramiro") {
+    console.log("It runs!")
+    age = 31
+  }
+
+  function incrementAge() {
+    age += 1
+  }
+  function changeName() {
+    name = "Jose Ramiro"
+  }
+  function nameInput(event) {
+    const enteredValue = event.target.value
+    name = enteredValue
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
-<button on:click="{incrementAge}">Change Age</button>
+<button on:click={incrementAge}>Change Age</button>
 <!-- <button on:click="{changeName}">Change Name</button> -->
-<input type="text" value="{name}" on:input="{nameInput}">
+<!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
+<input type="text" bind:value={name} />
+
+<ContactCard />
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
